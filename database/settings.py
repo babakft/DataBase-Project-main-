ce630ecbe4d30ff364c041d5624fc4e4710dad54
babak <babakfathi082@gmail.com>
2025-06-02 03:48:34 +0330

    last update.

--- database/settings.py
+++ database/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = ['192.168.131.64',"localhost" , '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.1.104',"localhost" , '127.0.0.1']
 
 
 # Application definition
